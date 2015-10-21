328ea24070b208d856219c778297fd8031bd5268
Saurabh Chauhan <chauhan.saurabh.b@gmail.com>
2015-10-21 09:57:37 +0530

    Added Support of  Periodic Sensor for Android

--- ToolSuite/src/iotsuite/parser/SymbolTable.java
+++ ToolSuite/src/iotsuite/parser/SymbolTable.java
@@ -157,6 +157,9 @@ public class SymbolTable {
 			symblTable.put(variableName, variableType);
 			
 		}
+		
+		
+		
 
 	}
 
--- ToolSuite/src/iotsuite/semanticmodel/ComputationalService.java
+++ ToolSuite/src/iotsuite/semanticmodel/ComputationalService.java
@@ -13,31 +13,19 @@ public class ComputationalService extends SoftwareComponent<Information> {
 	private Set<DataAccess> dataAccess = new HashSet<DataAccess>();
 	private Set<Action> actions = new HashSet<Action>();
 	private String operatation;
-/*	public static List<String> aggregatorStructFieldName = new ArrayList<String>();
-	public static List<String> aggregatorStructFieldType = new ArrayList<String>();*/
 	public List<StructField> fields = new ArrayList<StructField>();
 	public static int sampleValue;
-	/*
-	 * public ComputationalService(String name, Set<Attribute> attributes,
-	 * Set<Information> generateInfo, Set<Information> consumeInfo,
-	 * Set<DataAccess> dataAccess, Set<Action> actions, String
-	 * partitionAttributeValue) {
-	 */
+	
 
 	public ComputationalService(String name, Set<Information> generateInfo,
 			Set<Information> consumeInfo, Set<DataAccess> dataAccess,
 			Set<Action> actions, String partitionAttributeValue,
 			String operation,List<StructField> fields, int sampleValue) {
-		
-		 //List<String> aggregatorStructFieldName, List<String> aggregatorStructFieldType, 
-		// super(name, attributes, generateInfo, consumeInfo,
-		// partitionAttributeValue);
+				
 		super(name, generateInfo, consumeInfo, partitionAttributeValue);
 		this.dataAccess = dataAccess;
 		this.actions = actions;
 		this.operatation = operation;
-	/*	this.aggregatorStructFieldName=aggregatorStructFieldName;
-		this.aggregatorStructFieldType=aggregatorStructFieldType;*/
 		this.fields=fields;
 		this.sampleValue=sampleValue;
 	}
@@ -52,18 +40,6 @@ public class ComputationalService extends SoftwareComponent<Information> {
 		return allStructFields;
 	}
 
-	/*public List<String> getAllAggregatorStructFieldName()
-	{
-		//getAllAggregatorStructFieldType();
-		return aggregatorStructFieldName;	
-	}
-	
-	public List<String> getAllAggregatorStructFieldType()
-	{
-		
-		
-		return aggregatorStructFieldType;
-	}*/
 	
 	
 	public String getOperation() {
